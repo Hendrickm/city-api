795022bf579eac54f3bd5f4394925c90dd71e912
hendrickm <hendrick.lima@gmail.com>
2020-03-10 20:12:49 -0300

    Ajusta middleware

--- src/config/server.js
+++ src/config/server.js
@@ -1,12 +1,11 @@
 const port = 3003
 
-const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 const allowCors = require('./cors')
 
-server.use(bodyParser.urlencoded({ extended: true}))
-server.use(bodyParser.json())
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
 server.use(allowCors)
 
 server.listen(port, () => {
